1afaf894a6956e4217f2399e44580bae3a6a23b5
rafael oltra <rafaeloltra@gmail.com>
2023-02-16 16:21:56 -0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+ksqldb_key=$(/usr/bin/terraform output -raw ksqldb-api-key)
+ksqldb_secret=$(/usr/bin/terraform output -raw ksqldb-api-secret)
+ksqldb_endpoint=$(/usr/bin/terraform output -raw ksqldb-endpoint)
+ksqldb_sql_file="/host/create-users-stream.sql"
+
+echo running ksqldb-cli
+
+/usr/bin/docker run --volume="$(pwd):/host" --rm -it confluentinc/ksqldb-cli:0.28.2 ksql \
+     --file $ksqldb_sql_file  \
+     -u $ksqldb_key \
+     -p $ksqldb_secret \
+     $ksqldb_endpoint
