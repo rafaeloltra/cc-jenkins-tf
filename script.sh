8dce9749bb130bd74a744b6ec3825210b69379c7
rafael oltra <rafaeloltra@gmail.com>
2023-02-16 16:35:31 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ ksqldb_sql_file="/host/create-users-stream.sql"
 
 echo running ksqldb-cli
 
-/usr/bin/docker run --volume="$(pwd):/host" --rm -it confluentinc/ksqldb-cli:0.28.2 ksql \
+/usr/bin/docker run --volume="$(pwd):/host" --rm confluentinc/ksqldb-cli:0.28.2 ksql \
      --file $ksqldb_sql_file  \
      -u $ksqldb_key \
      -p $ksqldb_secret \
