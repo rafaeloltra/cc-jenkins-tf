73e57968c73bb5ca8377bc85b0d2c357b508ffc9
rafael oltra <rafaeloltra@gmail.com>
2023-02-16 19:44:04 -0500

    main w/ rbac

--- main.tf
+++ main.tf
@@ -39,19 +39,18 @@ resource "confluent_schema_registry_cluster" "essentials" {
 
 # Update the config to use a cloud provider and region of your choice.
 # https://registry.terraform.io/providers/confluentinc/confluent/latest/docs/resources/confluent_kafka_cluster
-resource "confluent_kafka_cluster" "standard" {
-  display_name = "rafa-tf-cluster"
+resource "confluent_kafka_cluster" "basic" {
+  display_name = "inventory"
   availability = "SINGLE_ZONE"
   cloud        = "AWS"
   region       = "us-east-2"
-  standard {}
+  basic {}
   environment {
     id = confluent_environment.staging.id
   }
 }
 
-// 'app-manager' service account is required in this configuration to grant ACLs
-// to 'app-ksql' service account and create 'users' topic
+// 'app-manager' service account is required in this configuration to create 'users' topic
 resource "confluent_service_account" "app-manager" {
   display_name = "rafa-tf-app-manager"
   description  = "Service account to manage 'inventory' Kafka cluster"
@@ -60,7 +59,7 @@ resource "confluent_service_account" "app-manager" {
 resource "confluent_role_binding" "app-manager-kafka-cluster-admin" {
   principal   = "User:${confluent_service_account.app-manager.id}"
   role_name   = "CloudClusterAdmin"
-  crn_pattern = confluent_kafka_cluster.standard.rbac_crn
+  crn_pattern = confluent_kafka_cluster.basic.rbac_crn
 }
 
 resource "confluent_api_key" "app-manager-kafka-api-key" {
@@ -73,9 +72,9 @@ resource "confluent_api_key" "app-manager-kafka-api-key" {
   }
 
   managed_resource {
-    id          = confluent_kafka_cluster.standard.id
-    api_version = confluent_kafka_cluster.standard.api_version
-    kind        = confluent_kafka_cluster.standard.kind
+    id          = confluent_kafka_cluster.basic.id
+    api_version = confluent_kafka_cluster.basic.api_version
+    kind        = confluent_kafka_cluster.basic.kind
 
     environment {
       id = confluent_environment.staging.id
@@ -96,256 +95,26 @@ resource "confluent_api_key" "app-manager-kafka-api-key" {
 
 resource "confluent_kafka_topic" "users" {
   kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
+    id = confluent_kafka_cluster.basic.id
   }
   topic_name    = "users"
-  rest_endpoint = confluent_kafka_cluster.standard.rest_endpoint
+  rest_endpoint = confluent_kafka_cluster.basic.rest_endpoint
   credentials {
     key    = confluent_api_key.app-manager-kafka-api-key.id
     secret = confluent_api_key.app-manager-kafka-api-key.secret
   }
 }
 
-// ksqlDB service account with only the necessary access
+// Service account that the ksqlDB cluster uses to talk to the Kakfa broker.
 resource "confluent_service_account" "app-ksql" {
   display_name = "rafa-tf-app-ksql"
-  description  = "Service account for Ksql cluster"
+  description  = "Service account to manage 'inventory' ksqlDB cluster"
 }
 
-resource "confluent_ksql_cluster" "main" {
-  display_name = "rafa-tf-ksql_cluster_0"
-  csu = 1
-  kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
-  }
-  credential_identity {
-    id = confluent_service_account.app-ksql.id
-  }
-  environment {
-    id = confluent_environment.staging.id
-  }
-
-  depends_on = [
-    confluent_schema_registry_cluster.essentials,
-    confluent_role_binding.app-ksql-schema-registry-resource-owner
-  ]
-}
-
-resource "confluent_kafka_acl" "app-ksql-describe-on-cluster" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
-  }
-  resource_type = "CLUSTER"
-  resource_name = "kafka-cluster"
-  pattern_type  = "LITERAL"
-  principal     = "User:${confluent_service_account.app-ksql.id}"
-  host          = "*"
-  operation     = "DESCRIBE"
-  permission    = "ALLOW"
-  rest_endpoint = confluent_kafka_cluster.standard.rest_endpoint
-  credentials {
-    key    = confluent_api_key.app-manager-kafka-api-key.id
-    secret = confluent_api_key.app-manager-kafka-api-key.secret
-  }
-}
-
-resource "confluent_kafka_acl" "app-ksql-describe-on-topic" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
-  }
-  resource_type = "TOPIC"
-  resource_name = "*"
-  pattern_type  = "LITERAL"
-  principal     = "User:${confluent_service_account.app-ksql.id}"
-  host          = "*"
-  operation     = "DESCRIBE"
-  permission    = "ALLOW"
-  rest_endpoint = confluent_kafka_cluster.standard.rest_endpoint
-  credentials {
-    key    = confluent_api_key.app-manager-kafka-api-key.id
-    secret = confluent_api_key.app-manager-kafka-api-key.secret
-  }
-}
-
-resource "confluent_kafka_acl" "app-ksql-describe-on-group" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
-  }
-  resource_type = "GROUP"
-  resource_name = "*"
-  pattern_type  = "LITERAL"
-  principal     = "User:${confluent_service_account.app-ksql.id}"
-  host          = "*"
-  operation     = "DESCRIBE"
-  permission    = "ALLOW"
-  rest_endpoint = confluent_kafka_cluster.standard.rest_endpoint
-  credentials {
-    key    = confluent_api_key.app-manager-kafka-api-key.id
-    secret = confluent_api_key.app-manager-kafka-api-key.secret
-  }
-}
-
-resource "confluent_kafka_acl" "app-ksql-describe-configs-on-cluster" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
-  }
-  resource_type = "CLUSTER"
-  resource_name = "kafka-cluster"
-  pattern_type  = "LITERAL"
-  principal     = "User:${confluent_service_account.app-ksql.id}"
-  host          = "*"
-  operation     = "DESCRIBE_CONFIGS"
-  permission    = "ALLOW"
-  rest_endpoint = confluent_kafka_cluster.standard.rest_endpoint
-  credentials {
-    key    = confluent_api_key.app-manager-kafka-api-key.id
-    secret = confluent_api_key.app-manager-kafka-api-key.secret
-  }
-}
-
-resource "confluent_kafka_acl" "app-ksql-describe-configs-on-topic" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
-  }
-  resource_type = "TOPIC"
-  resource_name = "*"
-  pattern_type  = "LITERAL"
-  principal     = "User:${confluent_service_account.app-ksql.id}"
-  host          = "*"
-  operation     = "DESCRIBE_CONFIGS"
-  permission    = "ALLOW"
-  rest_endpoint = confluent_kafka_cluster.standard.rest_endpoint
-  credentials {
-    key    = confluent_api_key.app-manager-kafka-api-key.id
-    secret = confluent_api_key.app-manager-kafka-api-key.secret
-  }
-}
-
-resource "confluent_kafka_acl" "app-ksql-describe-configs-on-group" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
-  }
-  resource_type = "GROUP"
-  resource_name = "*"
-  pattern_type  = "LITERAL"
-  principal     = "User:${confluent_service_account.app-ksql.id}"
-  host          = "*"
-  operation     = "DESCRIBE_CONFIGS"
-  permission    = "ALLOW"
-  rest_endpoint = confluent_kafka_cluster.standard.rest_endpoint
-  credentials {
-    key    = confluent_api_key.app-manager-kafka-api-key.id
-    secret = confluent_api_key.app-manager-kafka-api-key.secret
-  }
-}
-
-resource "confluent_kafka_acl" "app-ksql-describe-on-transactional-id" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
-  }
-  resource_type = "TRANSACTIONAL_ID"
-  resource_name = confluent_ksql_cluster.main.topic_prefix
-  pattern_type  = "LITERAL"
-  principal     = "User:${confluent_service_account.app-ksql.id}"
-  host          = "*"
-  operation     = "DESCRIBE"
-  permission    = "ALLOW"
-  rest_endpoint = confluent_kafka_cluster.standard.rest_endpoint
-  credentials {
-    key    = confluent_api_key.app-manager-kafka-api-key.id
-    secret = confluent_api_key.app-manager-kafka-api-key.secret
-  }
-}
-
-resource "confluent_kafka_acl" "app-ksql-write-on-transactional-id" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
-  }
-  resource_type = "TRANSACTIONAL_ID"
-  resource_name = confluent_ksql_cluster.main.topic_prefix
-  pattern_type  = "LITERAL"
-  principal     = "User:${confluent_service_account.app-ksql.id}"
-  host          = "*"
-  operation     = "WRITE"
-  permission    = "ALLOW"
-  rest_endpoint = confluent_kafka_cluster.standard.rest_endpoint
-  credentials {
-    key    = confluent_api_key.app-manager-kafka-api-key.id
-    secret = confluent_api_key.app-manager-kafka-api-key.secret
-  }
-}
-
-resource "confluent_kafka_acl" "app-ksql-all-on-topic-prefix" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
-  }
-  resource_type = "TOPIC"
-  resource_name = confluent_ksql_cluster.main.topic_prefix
-  pattern_type  = "PREFIXED"
-  principal     = "User:${confluent_service_account.app-ksql.id}"
-  host          = "*"
-  operation     = "ALL"
-  permission    = "ALLOW"
-  rest_endpoint = confluent_kafka_cluster.standard.rest_endpoint
-  credentials {
-    key    = confluent_api_key.app-manager-kafka-api-key.id
-    secret = confluent_api_key.app-manager-kafka-api-key.secret
-  }
-}
-
-resource "confluent_kafka_acl" "app-ksql-all-on-topic-confluent" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
-  }
-  resource_type = "TOPIC"
-  resource_name = "_confluent-ksql-${confluent_ksql_cluster.main.topic_prefix}"
-  pattern_type  = "PREFIXED"
-  principal     = "User:${confluent_service_account.app-ksql.id}"
-  host          = "*"
-  operation     = "ALL"
-  permission    = "ALLOW"
-  rest_endpoint = confluent_kafka_cluster.standard.rest_endpoint
-  credentials {
-    key    = confluent_api_key.app-manager-kafka-api-key.id
-    secret = confluent_api_key.app-manager-kafka-api-key.secret
-  }
-}
-
-resource "confluent_kafka_acl" "app-ksql-all-on-group-confluent" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
-  }
-  resource_type = "GROUP"
-  resource_name = "_confluent-ksql-${confluent_ksql_cluster.main.topic_prefix}"
-  pattern_type  = "PREFIXED"
-  principal     = "User:${confluent_service_account.app-ksql.id}"
-  host          = "*"
-  operation     = "ALL"
-  permission    = "ALLOW"
-  rest_endpoint = confluent_kafka_cluster.standard.rest_endpoint
-  credentials {
-    key    = confluent_api_key.app-manager-kafka-api-key.id
-    secret = confluent_api_key.app-manager-kafka-api-key.secret
-  }
-}
-
-# Topic specific permissions. You have to add an ACL like this for every Kafka topic you work with.
-resource "confluent_kafka_acl" "app-ksql-all-on-topic" {
-  kafka_cluster {
-    id = confluent_kafka_cluster.standard.id
-  }
-  resource_type = "TOPIC"
-  resource_name = confluent_kafka_topic.users.topic_name
-  pattern_type  = "PREFIXED"
-  principal     = "User:${confluent_service_account.app-ksql.id}"
-  host          = "*"
-  operation     = "ALL"
-  permission    = "ALLOW"
-  rest_endpoint = confluent_kafka_cluster.standard.rest_endpoint
-  credentials {
-    key    = confluent_api_key.app-manager-kafka-api-key.id
-    secret = confluent_api_key.app-manager-kafka-api-key.secret
-  }
+resource "confluent_role_binding" "app-ksql-kafka-cluster-admin" {
+  principal   = "User:${confluent_service_account.app-ksql.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.basic.rbac_crn
 }
 
 resource "confluent_role_binding" "app-ksql-schema-registry-resource-owner" {
@@ -354,12 +123,23 @@ resource "confluent_role_binding" "app-ksql-schema-registry-resource-owner" {
   crn_pattern = format("%s/%s", confluent_schema_registry_cluster.essentials.resource_name, "subject=*")
 }
 
-# ACLs are needed for KSQL service account to read/write data from/to kafka, this role instead is needed for giving
-# access to the Ksql cluster.
-resource "confluent_role_binding" "app-ksql-ksql-admin" {
-  principal   = "User:${confluent_service_account.app-ksql.id}"
-  role_name   = "KsqlAdmin"
-  crn_pattern = "${confluent_kafka_cluster.standard.rbac_crn}/ksql=${confluent_ksql_cluster.main.id}"
+resource "confluent_ksql_cluster" "main" {
+  display_name = "rafa-tf-ksql_cluster_0"
+  csu = 1
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  credential_identity {
+    id = confluent_service_account.app-ksql.id
+  }
+  environment {
+    id = confluent_environment.staging.id
+  }
+  depends_on = [
+    confluent_role_binding.app-ksql-kafka-cluster-admin,
+    confluent_role_binding.app-ksql-schema-registry-resource-owner,
+    confluent_schema_registry_cluster.essentials
+  ]
 }
 
 resource "confluent_api_key" "app-ksqldb-api-key" {
